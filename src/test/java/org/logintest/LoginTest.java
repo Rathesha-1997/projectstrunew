5e4bb831ee0b55d0ff400463ae9b0683665ea83b
Rathesha-1997 <ratheeraffi97@gmail.com>
2021-12-01 14:54:22 +0530

    new rarhi

--- src/test/java/org/logintest/LoginTest.java
+++ src/test/java/org/logintest/LoginTest.java
@@ -1,7 +1,13 @@
 package org.logintest;
 
 public class LoginTest {
+	private void emp() {
+		System.out.println("empl name is rathi");
+		
+	}
 	public static void main(String[] args) {
+		
+		
 		
 	}
 }
